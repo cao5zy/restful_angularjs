9d4cdb68e9a7445fc702659b8dcaaa4430a6d0b4
caozy <cao5zy@163.com>
2017-11-17 19:25:09 +0800

    add autofac

---
+++ backend/UserService/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using Autofac;
+using Autofac.Integration.Wcf;
+
+namespace UserService
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AutofacHostFactory.Container = new Func<ContainerBuilder, IContainer>((builder) =>
+            {
+                builder.RegisterType<global::UserService.UserService>();
+                return builder.Build();
+            })(new ContainerBuilder());
+        }
+
+    }
+}
--- backend/UserService/UserService.svc.cs
+++ backend/UserService/UserService.svc.cs
@@ -12,7 +12,7 @@ namespace UserService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IUserService
+    public class UserService : IUserService
     {
         public void CreateUser(User user)
         {
