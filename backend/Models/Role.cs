5f7fb407a5da90519872ea9a848d936fb40fed75
Cao <zongying.cao@hpe.com>
2017-12-01 02:26:21 +0000

    improve model

--- backend/Models/Role.cs
+++ backend/Models/Role.cs
@@ -13,22 +13,9 @@ namespace Models
     using System;
     using System.Collections.Generic;
     
-    using System.Runtime.Serialization;
-    [DataContract]
     public partial class Role
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Role()
-        {
-            this.User = new HashSet<User>();
-        }
-    
-    	[DataMember]
         public int RoleId { get; set; }
-    	[DataMember]
         public string Name { get; set; }
-    
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<User> User { get; set; }
     }
 }
