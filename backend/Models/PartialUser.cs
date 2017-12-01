d29ceebc7102dd9448e202ba322cd11fbddf3b1d
Cao <zongying.cao@hpe.com>
2017-12-01 14:17:13 +0000

    handle the time output

---
+++ backend/Models/PartialUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public partial class User
+    {
+
+        [OnSerializing]
+        void OnSerializing(StreamingContext ctx)
+        {
+            this.DateOfBirthStr = this.DateOfBirth != null ? this.DateOfBirth.Value.ToShortDateString() : ""; 
+        }
+        public string DateOfBirthStr { get; set; }
+    }
+}
