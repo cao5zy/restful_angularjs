8fd0e276cb6a000c40b7ab540fa1a3b5fff2f57a
Zongying Cao <zongying.cao@hpe.com>
2017-12-11 14:58:58 +0800

    get value to be validated

--- frontend/src/app/user-name-rule.directive.ts
+++ frontend/src/app/user-name-rule.directive.ts
@@ -1,10 +1,16 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, Validator, ValidatorFn, Validators } from '@angular/forms';
 
 @Directive({
-  selector: '[appUserNameRule]'
+  selector: '[appUserNameRule]',
+  providers: [{provide: NG_VALIDATORS, useExisting: UserNameRuleDirective, multi: true}]
 })
-export class UserNameRuleDirective {
+export class UserNameRuleDirective implements Validator {
 
   constructor() { }
 
+  validate(control: AbstractControl):{[key: string]: any}{
+  	console.log('validate:', control.value);
+  	return null;
+  }
 }
