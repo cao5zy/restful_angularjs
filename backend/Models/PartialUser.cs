99eda31c04543d903413f6b6a010daed57d1f98e
Zongying Cao <zongying.cao@hpe.com>
2017-12-14 17:04:54 +0800

    handle date

--- backend/Models/PartialUser.cs
+++ backend/Models/PartialUser.cs
@@ -14,7 +14,12 @@ namespace Models
         [OnSerializing]
         void OnSerializing(StreamingContext ctx)
         {
-            this.DateOfBirthStr = this.DateOfBirth != null ? this.DateOfBirth.Value.ToShortDateString() : ""; 
+            this.DateOfBirthStr = this.DateOfBirth != null ? this.DateOfBirth.Value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture) : ""; 
+        }
+        [OnDeserialized]
+        void OnDeserializing(StreamingContext ctx)
+        {
+            this.DateOfBirth = DateTime.ParseExact(this.DateOfBirthStr, "yyyy-MM-dd", CultureInfo.InvariantCulture);
         }
         public string DateOfBirthStr { get; set; }
     }
