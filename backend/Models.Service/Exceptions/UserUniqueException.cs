a4c53627fab7037e98df07e2531850e6cbc3cc99
Zongying Cao <zongying.cao@hpe.com>
2017-12-14 20:27:07 +0800

    add exception

---
+++ backend/Models.Service/Exceptions/UserUniqueException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Service.Exceptions
+{
+    public class UserUniqueException :Exception
+    {
+        public UserUniqueException(string userName) : base($"{ userName} has been existed. ")
+        {}
+    }
+}
